de23999633a8c3ccf4c030cb0e08067bea6c41b1
Dhiraj kumar Nayak <dnayak696@gmail.com>
2023-05-28 14:25:18 +0530

    closure vs callbacks

---
+++ closurevsCallback.js
@@ -0,0 +1,41 @@
+function sayHi (){
+    var greeting ="Hi!";
+
+    setTimeout( function(){
+       console.log( greeting );
+    },3000)
+}
+
+sayHi()
+
+
+
+// setTimeout is a callback function
+// Her set timeout is a clousure
+
+
+
+// IN JQuery
+// $("button").click(function(){
+//   
+//}) 
+
+
+/**
+ * CALLBACK FUNCTIONS
+ */
+
+function tellMeWhenDone(callback) {
+     var a = 1000
+     var b = 2000
+
+     callback();
+}
+
+
+tellMeWhenDone(function(){
+    console.log("I am Done")
+});
+tellMeWhenDone(function(){
+    console.log("All Done")
+});
