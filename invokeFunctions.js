d851eaf2cb27117f9d79973f4ff2acb9c2dc3141
Dhiraj kumar Nayak <dnayak696@gmail.com>
2023-05-28 09:31:52 +0530

    framework aside iife

--- invokeFunctions.js
+++ invokeFunctions.js
@@ -11,7 +11,29 @@ greetFunc('Jhon');
 
 var greeting = function (name){
     return 'Hello '+ name;
-}("Rock")
-
+}("Rock");
 
 console.log(greeting);
+
+
+"i am a string" // valid not getting any error
+3; // A valid js expression
+
+// function(name){
+//     name: "uhaoi"
+// }//  this is not a valid function
+
+(function(name){
+    console.log(name);
+    return "Hello "+ name;
+}("Biki")) //classic example of IIFE
+
+
+
+// Frame work Aside
+
+console.log("Function Aside");
+
+
+// Global exeecution context 
+// Execution Context for the anonymous function // getting "Hello"
